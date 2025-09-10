86311edc6a267122b056688602572220f7cc1ec1
VikasKandhal <140228156+VikasKandhal@users.noreply.github.com>
2025-09-10 22:14:46 +0530

    reflect first commits

---
+++ components/header.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+
+const Header = () => {
+  return <header className="container mx-auto">
+     <nav className="py-6 px-4 flex justify-between items-center">
+     <Link href={"/"}>
+     <Image 
+      src={"/logo.png"} 
+      alt="Reflct Logo" 
+      width={200} 
+      height={60}
+      className="h-10 w-auto object-contain"
+     />
+     </Link>
+
+     <div className="flex items-center gap-4" >
+        
+          <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            
+     </div>
+
+     </nav>
+
+  </header>;
+};
+
+
+export default Header;
